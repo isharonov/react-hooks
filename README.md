3963f014919348ddf469ae04fefd295b6e75522f
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2022-01-14 12:32:44 +0300

    9-96: New project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hooks React App
+
+Study project within the course [React + Redux - Professional Development](https://www.udemy.com/course/pro-react-redux/).
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <p>React Hooks App</p>
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
